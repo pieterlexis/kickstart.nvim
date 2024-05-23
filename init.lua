3aac00d0675ae267f976e38d3419fe64986eeb84
Pieter Lexis <pieter.lexis@tomtom.com>
2024-05-23 17:00:22 +0200

    Add Markdown parsers and linters

--- init.lua
+++ init.lua
@@ -519,16 +519,8 @@ require('lazy').setup({
         terraformls = {},
         tflint = {},
         yamlls = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
+        markdownlint = {},
+        marksman = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -789,10 +781,11 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-context',
+      'tree-sitter-grammars/tree-sitter-markdown',
     },
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
