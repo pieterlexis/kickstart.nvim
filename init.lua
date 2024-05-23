8fae3220fdaef8deef9b4fe76b0642ac150b9643
Pieter Lexis <pieter.lexis@tomtom.com>
2024-05-23 11:52:48 +0200

    Add tflint lang server

--- init.lua
+++ init.lua
@@ -516,6 +516,7 @@ require('lazy').setup({
         gopls = {},
         pyright = {},
         terraformls = {},
+        tflint = {},
         yamlls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
