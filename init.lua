ab3898d14c826e46990be8d4cfba07b1d24e133d
Pieter Lexis <pieter@plexis.eu>
2026-06-14 12:43:10 +0200

    Add LaTeX

--- init.lua
+++ init.lua
@@ -658,6 +658,7 @@ require('lazy').setup({
         },
         ruff = {},
         tflint = {},
+        texlab = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
